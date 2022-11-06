77c7a5d1f59e344f2d2cf540ef10a66f328baa4d
Sudhanshu Chaudhary <sudhanshulicensed>
2022-11-06 18:16:13 +0530

    Initial Push

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex);
+
+export default Vuex.Store({
+    state: {
+        listofUsers: [],
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+})
